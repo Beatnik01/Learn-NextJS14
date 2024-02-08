fa87c37c02c875c4c84b29a7959314fd89681a93
Gihoon Lim <knv429@naver.com>
2024-02-08 21:16:10 +0900

    Suspense 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,20 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
 export const metadata = {
   title: "Movie",
 };
 
-export default function Movie({ params: { id } }: { params: { id: string } }) {
-  console.log({ id });
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
   return (
     <div>
-      <h1>Movie {id}</h1>
+      <Suspense fallback={<h1>Loading Movie Info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading Movie Videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
     </div>
   );
 }
